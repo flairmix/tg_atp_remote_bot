b66969ce5cd1bfc995b27ce52c9674592c5bfcd9
flairmix <d9067246810@gmail.com>
2025-01-22 16:12:26 +0000

    approving grl function work

--- api/database_connection.py
+++ api/database_connection.py
@@ -7,10 +7,6 @@ from dataclasses import dataclass
 
 from atp_users_hkls import users_hkls
 
-#sqlite
-# engine = create_engine("sqlite:///tutorial.db", echo=True)
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 #postgress
 DATABASE_URL = os.getenv("DATABASE_URL")
 CHAT_ID_KIRA = os.getenv("CHAT_ID_KIRA")
@@ -38,48 +34,6 @@ for user in users_hkls:
                          chat_id_grl = user['chat_id_grl'])
     
     person_list.append(user_current)
-
-#testers db
-# person_list = []
-
-# user1 = Users(id = "TEST", name="TEST", surname="Donchenko", email="TEST.donchenko@atp-tlp.ru", 
-#     group="DEV", chat_id=432923144, GRL="KIRA", level="TL2", city="MOS", chat_id_grl=CHAT_ID_KIRA)
-
-# user1_1 = Users(id = "MID", name="Михаил", surname="Donchenko", email="TEST.donchenko@atp-tlp.ru", 
-#     group="DEV", chat_id=432923144, GRL="KIRA", level="TL2", city="MOS", chat_id_grl=CHAT_ID_KIRA)
-
-# user2 =  Users(id = "GUV", name="Vsevolod", surname="Gurov", email="Vsevolod.Gurov@atp-tlp.ru", 
-#     group="BIM", chat_id=0, GRL="KIRA", level="TL1", city="MOS", chat_id_grl=CHAT_ID_KIRA)
-
-# user3 = Users(id = "SHKA", name="Egor", surname="Shkaev", email="Egor.Shkaev@atp-tlp.ru", 
-#     group="BIM", chat_id=0, GRL="KIRA", city="SPB", level="TL1", chat_id_grl=CHAT_ID_KIRA)
-
-# user4 = Users(id = "INI", name="Nikita", surname="Ignatiev", email="Nikita.Ignatiev@atp-tlp.ru", 
-#     group="OV", chat_id=0, GRL="KIRA", level="PRAK", city="MOS", chat_id_grl=CHAT_ID_KIRA)
-
-# user5 = Users(id = "ADL", name="Alexander", surname="Dolinskiy", email="Alexander.Dolinskiy@atp-tlp.ru", 
-#     group="OV", chat_id=0, GRL="KIRA", level="PRAK", city="MOS", chat_id_grl=CHAT_ID_KIRA)
-
-# user6 = Users(id = "PDE", name="Fedor", surname="Perkhalyuk", email="Fedor.Perkhalyuk@atp-tlp.ru", 
-#     group="VK", chat_id=0, GRL="KIRA", level="-", city="MOS", chat_id_grl=CHAT_ID_KIRA)
-
-# user7 = Users(id = "VAN", name="Anastasia", surname="Volodina", email="Anastasia.Volodina@atp-tlp.ru", 
-#     group="VK", chat_id=0, GRL="KIRA", level="-", city="MOS", chat_id_grl=CHAT_ID_KIRA)
-
-# user8 = Users(id = "KIRA", name="Andrey", surname="Kirpichnikov", email="Andrey.Kirpichnikov@atp-tlp.ru", 
-#     group="HKLS", chat_id=0, GRL="KIRA", level="GRL", city="MOS", chat_id_grl=CHAT_ID_KIRA)
-
-
-# person_list.append(user1)
-# person_list.append(user1_1)
-# person_list.append(user2)
-# person_list.append(user3)
-# person_list.append(user4)
-# person_list.append(user5)
-# person_list.append(user6)
-# person_list.append(user7)
-# person_list.append(user8)
-
 
 with session1:
 
--- api/users/controller.py
+++ api/users/controller.py
@@ -10,6 +10,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import text, select, update
 from sqlalchemy.exc import SQLAlchemyError
 
+from services.tg_api import send_message
 
 # Dependency to provide a database session
 def get_db() -> Generator[Session, None, None]:
@@ -206,6 +207,21 @@ class UserController(Controller):
         try:
             db.execute(update(User_requests).where(User_requests.id == request_id).values(approve_grl=True))
             db.commit()
+
+            request = select(User_requests).where(User_requests.id == request_id)
+            user_request = db.execute(request).scalars().first()
+
+            user = select(Users).where(Users.id == user_request.user_id)
+            user_chat_id = db.execute(user).scalars().first().chat_id
+            
+            message = (
+                f"Ваш запрос на {user_request.work_status} \n" + 
+                f"для даты {user_request.date_for_request} \n" +
+                "Одобрен"
+                )
+
+            await send_message(user_chat_id, message)
+
             
             return Response(status_code=200, content=f"Request {request_id} approved successfully.")
         
@@ -245,4 +261,9 @@ class UserController(Controller):
         else:
             return Response(status_code=204,
                             content=f"Users <{id}> not found")
-                            
+
+
+# @listener("user_request_approved")
+# async def send_approve_handler(self, chat_id: int) -> None:
+
+#     await tg_app.
